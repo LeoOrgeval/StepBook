fe572e4aa704f092caa989b1720ee0399f355cb8
Leo Orgeval <leoorgeval.pro@gmail.com>
2024-09-19 11:13:04 +0200

    For push

--- step-book/src/app/app.module.ts
+++ step-book/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {importProvidersFrom, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,7 +8,18 @@ import {FooterComponent} from "./footer/footer.component";
 import { CardComponent } from './card/card.component';
 import { FormLoginComponent } from './form-login/form-login.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCp9Gykp9tbc6oA-nlm-D_8zYashevCpHo",
+  authDomain: "step-book.firebaseapp.com",
+  databaseURL: "https://step-book-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "step-book",
+  storageBucket: "step-book.appspot.com",
+  messagingSenderId: "710161700171",
+  appId: "1:710161700171:web:2d188cffd5cf435cc004ce"
+};
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,9 +33,12 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth())
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
