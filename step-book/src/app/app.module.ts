9dd83eb38325ebb57f388a323639344d605d3c33
Leo Orgeval <leoorgeval.pro@gmail.com>
2024-09-19 16:04:40 +0200

    Fix Database & Hosting & Authentication

--- step-book/src/app/app.module.ts
+++ step-book/src/app/app.module.ts
@@ -1,43 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import {FooterComponent} from "./footer/footer.component";
+import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
 import { ProfileComponent } from './profile/profile.component';
 import { CardComponent } from './card/card.component';
-
 import { FormLoginComponent } from './form-login/form-login.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { FormSignupComponent } from './form-signup/form-signup.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Firebase modules (avec la nouvelle API)
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
-import { FormSignupComponent } from './form-signup/form-signup.component';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { environment } from '../environnements/environnement'; // Assurez-vous que votre environnement est correct
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCp9Gykp9tbc6oA-nlm-D_8zYashevCpHo",
-  authDomain: "step-book.firebaseapp.com",
-  databaseURL: "https://step-book-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "step-book",
-  storageBucket: "step-book.appspot.com",
-  messagingSenderId: "710161700171",
-  appId: "1:710161700171:web:2d188cffd5cf435cc004ce"
-};
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    NavbarComponent,
     CardComponent,
     FormLoginComponent,
     FormSignupComponent,
-    FormLoginComponent,
-    CardComponent,
     LoginComponent,
     RegisterComponent,
     WishlistComponent,
@@ -48,13 +39,15 @@ const firebaseConfig = {
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule,
     HttpClientModule,
+
   ],
   providers: [
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideAuth(() => getAuth())
-],
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
