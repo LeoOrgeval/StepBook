ef33d5a1bbfbf4a9e7be7caffd5c74457b5ef2d9
Tom Bihoreau <82575721+tombihoreau@users.noreply.github.com>
2024-09-19 09:37:05 +0200

    navbar

--- step-book/src/app/card/card.component.ts
+++ step-book/src/app/card/card.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent {
+
+export class CardComponent{
 
 }
